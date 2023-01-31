74050dc5a764478b654754aaedb0a43e6b500184
Abishaya-Pavalasingam <abishaya300@gmail.com>
2023-01-31 19:21:32 +0530

    2019CSC028

--- java/Circle.java
+++ java/Circle.java
@@ -3,3 +3,17 @@ Problem:
 Write a Java program to print the area and perimeter of a circle
 
 */
+
+public class Exercise2 {
+ 
+   private static final double radius = 7.5;
+
+    public static void main(String[] args) {
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Perimeter is = " + perimeter);
+        System.out.println("Area is = " + area);
+    }
+}
